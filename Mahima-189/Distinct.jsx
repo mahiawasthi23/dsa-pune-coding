3d15a5d8ca982c7f63440fa65d8bb0f999fca25c
mahiawasthi23 <mahima23@navgurukul.org>
2025-06-05 12:35:34 +0530

    codeChefcode

---
+++ Mahima-189/Distinct.jsx
@@ -0,0 +1,37 @@
+// #include <stdio.h>
+
+// int main() {
+//    int t,n;
+//    scanf("%d",&t);
+//    while(t--){
+//        scanf("%d",&n);
+//        int a[n];
+//        for(int i=0; i<n; i++){
+//            scanf("%d",&a[i]);
+//        }
+//        for(int j=0; j<n-1; j++){
+//            for(int k=j+1; k<n; k++){
+//                if(a[j]>a[k]){
+//                    int d = a[j];
+//                    a[j] = a[k];
+//                    a[k] = d;
+//                 }
+//            }
+//        }
+  
+//       int count = 0;
+//       for(int c=0; c<n-1; c++){
+//           if(a[c]!= a[c+1]){
+//               count++;
+//           }
+//       }
+//       if(count >= 1){
+//           printf("2\n");
+//       }else{
+//           printf("-1\n");
+//       }
+     
+//     }
+//     return 0;
+// }
+
---
+++ Mahima-189/Pizz.jsx
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+
+// int main() {
+// 	int a,b;
+// 	scanf("%d%d",&a,&b);
+// 	int c = (a+1)*4;
+// 	int d = b*3;
+// 	int k = c+d;
+// 	if(k%8 == 0){
+// 	    printf("%d",k/8);
+// 	}else{
+// 	    printf("%d",(k/8)+1);
+// 	}
+// 	return 0;
+// }
+
